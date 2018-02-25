19e8a6a41ca6ba0d57cb98292b2572f2ef69e306
Marshal J. Dickey <mdickey14@gmail.com>
2018-02-25 18:10:53 -0500

    Creating the NLPForHealthValue and QueryWikapediaForDescription classes

---
+++ src/main/java/org/gnomerous/ingr3/process/chain/handler/NLPForHealthValue.java
@@ -0,0 +1,9 @@
+package org.gnomerous.ingr3.process.chain.handler;
+
+public class NLPForHealthValue extends HealthValueHandler {
+
+    @Override
+    protected double lookupHealthValue(String name) {
+        return 0;
+    }
+}
---
+++ src/main/java/org/gnomerous/ingr3/process/chain/handler/QueryWikapediaForDescription.java
@@ -0,0 +1,10 @@
+package org.gnomerous.ingr3.process.chain.handler;
+
+public class QueryWikapediaForDescription extends DescriptionHandler {
+
+    @Override
+    protected String lookupDescription(String name) {
+        return null;
+    }
+
+}
