8afb81cfc9f43125918e466c76623168c2bed597
Marshal J. Dickey <mdickey14@gmail.com>
2018-02-25 17:53:43 -0500

    Initial Health Value Handler

--- src/main/java/org/gnomerous/ingr3/process/chain/handler/HealthValueHandler.java
+++ src/main/java/org/gnomerous/ingr3/process/chain/handler/HealthValueHandler.java
@@ -1,5 +1,23 @@
 package org.gnomerous.ingr3.process.chain.handler;
 
-public class HealthValueHandler {
+import org.gnomerous.ingr3.model.Ingredient;
 
+public abstract class HealthValueHandler extends IngredientHandler {
+
+    @Override
+    public void process(Ingredient ingredient) {
+
+        // Lookup the healthValue
+        double healthValue = lookupHealthValue(ingredient.getIngredientName());
+
+        // Validate healthValue
+        if (healthValue == Ingredient.DEFAULT_RANKING) { 
+            // TODO:
+
+        } else { 
+            ingredient.setRanking(healthValue);
+        }
+    }
+
+    protected abstract double lookupHealthValue(String name);
 }
