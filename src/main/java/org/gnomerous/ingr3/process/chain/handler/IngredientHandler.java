09a7d1ebb4030c8d6aa230b19a8fafdb83256d70
Marshal J. Dickey <mdickey14@gmail.com>
2018-02-25 17:34:53 -0500

    Initial Ingredient Handler

--- src/main/java/org/gnomerous/ingr3/process/chain/handler/IngredientHandler.java
+++ src/main/java/org/gnomerous/ingr3/process/chain/handler/IngredientHandler.java
@@ -1,5 +1,25 @@
 package org.gnomerous.ingr3.process.chain.handler;
 
-public interface IngredientHandler {
+// INGR3
+import org.gnomerous.ingr3.model.Ingredient;
 
+public abstract class IngredientHandler {
+
+    private IngredientHandler successor;
+    private boolean hasSuccessor = false;
+
+    public void setSuccessor(IngredientHandler s) {
+        hasSuccessor = true;
+        this.successor = s;
+    }
+
+    public abstract void process(Ingredient ingredient);
+
+    protected boolean hasSuccessor() { 
+        return hasSuccessor;
+    }
+
+    protected IngredientHandler getSuccessor() { 
+        return successor;
+    }
 }
