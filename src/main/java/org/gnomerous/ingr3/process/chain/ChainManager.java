8763fe08eb1efce9b02342f0095714e697ac8942
Marshal J. Dickey <mdickey14@gmail.com>
2018-02-25 17:35:18 -0500

    Initial Chain Manager

--- src/main/java/org/gnomerous/ingr3/process/chain/ChainManager.java
+++ src/main/java/org/gnomerous/ingr3/process/chain/ChainManager.java
@@ -1,5 +1,27 @@
 package org.gnomerous.ingr3.process.chain;
 
+// JAVA
+import java.util.ArrayList;
+import java.util.List;
+
+// INGR3
+import org.gnomerous.ingr3.model.Ingredient;
+import org.gnomerous.ingr3.process.chain.handler.IngredientHandler;
+
 public class ChainManager {
 
+    private List<IngredientHandler> chain = new ArrayList<>();
+    
+    public void appendToChain(IngredientHandler ih) { 
+        this.chain.add(ih);
+    }
+
+    public void clearChain() { 
+        chain.clear();
+    }
+
+    public boolean runChainOnIngredient(Ingredient ingredient) { 
+        chain.forEach(ih -> ih.process(ingredient));
+        return true;
+    }
 }
